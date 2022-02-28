6eaa3fa0b716440164d376f85481e7ac5afc2c6a
John Doe <Alexandrrr999@mail.ru>
2022-02-28 14:16:12 +0500

    k

--- controller.py
+++ controller.py
@@ -1,12 +1,12 @@
 import umn as model
 import view 
 import init
+import Z
 
 
 def button_click():
     value_a = view.get_value()
     value_b = view.get_value()
     init.init_num(value_a,value_b)
-    print(init.get_data())
-    result = model.umn()
+    result = Z.znach()
     view.view_data(result)
